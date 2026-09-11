fc665fcee9ad0597b4772a89545f660ed5c2744d
tolokimai <tolokimai@gmail.com>
2026-09-11 14:46:46 +0700

    chore: bump app version to 9.0
    Increment version code to 9 and version name to 9.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.autopoststudio.pxqw"
     minSdk = 24
     targetSdk = 36
-    versionCode = 8
-    versionName = "8.0"
+    versionCode = 9
+    versionName = "9.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
