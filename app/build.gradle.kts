ecf2fdfae4eb697f6f06b2168b6ae9908fe557da
tolokimai <tolokimai@gmail.com>
2026-09-11 14:33:08 +0700

    build: bump version to 8.0
    Update versionCode to 8 and versionName to 8.0 for the next release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.autopoststudio.pxqw"
     minSdk = 24
     targetSdk = 36
-    versionCode = 7
-    versionName = "7.0"
+    versionCode = 8
+    versionName = "8.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
