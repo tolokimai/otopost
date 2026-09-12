592f370c2ef998c09b67fd3cda1d46ad53d69ba6
tolokimai <tolokimai@gmail.com>
2026-09-13 04:11:45 +0700

    chore: bump version to 29.0 (build marker for Step 5 podcast fix)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.autopoststudio.pxqw"
     minSdk = 24
     targetSdk = 36
-    versionCode = 28
-    versionName = "28.0"
+    versionCode = 29
+    versionName = "29.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
