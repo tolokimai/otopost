632bbb1135813ec3e1450b08c34eba493e0ffbbc
tolokimai <tolokimai@gmail.com>
2026-09-13 03:18:59 +0700

    chore: bump version to 28.0 (build verification marker for Podcast Clip v2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.autopoststudio.pxqw"
     minSdk = 24
     targetSdk = 36
-    versionCode = 27
-    versionName = "27.0"
+    versionCode = 28
+    versionName = "28.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
