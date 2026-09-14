ac214abf2cb4531a9be6e7e5b7889f252e5d42c3
tolokimai <tolokimai@gmail.com>
2026-09-14 13:31:09 +0700

    chore: bump versi ke 36 (remake async + GPU config)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.autopoststudio.pxqw"
     minSdk = 24
     targetSdk = 36
-    versionCode = 35
-    versionName = "35.0"
+    versionCode = 36
+    versionName = "36.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
