9cd5fac1cd1871de830129654c070d64ded0d1b4
tolokimai <tolokimai@gmail.com>
2026-09-14 09:19:03 +0700

    chore: bump versi ke 34.0 (code 34)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.autopoststudio.pxqw"
     minSdk = 24
     targetSdk = 36
-    versionCode = 33
-    versionName = "33.0"
+    versionCode = 34
+    versionName = "34.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
